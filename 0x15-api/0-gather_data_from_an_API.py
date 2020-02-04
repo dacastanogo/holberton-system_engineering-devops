5a7aaa675e37ffe82a8ff81f584ff7fad2fda9f5
dacastanogo <dcastanogomez@gmail.com>
2020-02-03 23:25:08 -0500

    Fixed output in Task 0 and 1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,22 +7,20 @@ from sys import argv
 if __name__ == '__main__':
     """Request from an API"""
     employee_id = argv[1]
-    url_todo = 'https://jsonplaceholder.typicode.com/todos?userId='
-    url_user = 'https://jsonplaceholder.typicode.com/users/'
-    todo = requests.get(url_todo + employee_id).json()
-    user = requests.get(url_user + employee_id).json()
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                            format(employee_id))
+    name = user.json()['name']
+    all_todo = 'https://jsonplaceholder.typicode.com/all_todo?userId={}'\
+        .format(employee_id)
+
+    tasks = requests.get("{}".format(all_todo)).json()
     done_tasks = []
-    done_count = 0
-    total_tasks = len(todo)
-    employee = user.get('name')
-
-    for task in todo:
-        if task.get('completed'):
+    for task in tasks:
+        if task['completed'] is True:
             done_tasks.append(task)
-            done_count += 1
-
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employee, done_count, total_tasks))
-
+    total_tasks = len(tasks)
+    done_count = len(done_tasks)
+    print("Employee {} is done with tasks({}/{}):".
+          format(name, done_count, total_tasks))
     for task in done_tasks:
         print("\t {}".format(task['title']))
--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,49 +1,25 @@
-#!/usr/bin/pyton3
-"""Return information about employee TODO list progress"""
+#!/usr/bin/python3
+"""Return information about employee todo list, export to CSV"""
+import json
 import requests
-import sys
-import csv
-
-requested_url = "https://jsonplaceholder.typicode.com/"
+from sys import argv
 
 
-def api_request():
-    """Request from an API"""
-    if len(sys.argv) < 2:
-        return print('USAGE:', __file__, '<employee id>')
-    employee_id = sys.argv[1]
-    try:
-        _employee_id = int(sys.argv[1])
-    except ValueError:
-        return print("Employee id must be an integer")
-
-    response = requests.get(requested_url + 'users/' + employee_id)
-    if response.status_code == 404:
-        return print('User id not found')
-    elif response.status_code != 200:
-        return print('Error: status_code:', response.status_code)
-    user = response.json()
-
-    response = requests.get(requested_url + 'todos/')
-    if response.status_code != 200:
-        return print('Error: status_code:', response.status_code)
-    todos = response.json()
-
-    user_todos = [todo for todo in todos
-                  if todo.get('userId') == user.get('id')]
-    completed = [todo for todo in user_todos if todo.get('completed')]
-    print('Employee', user.get('name'),
-          'is done with tasks({}/{}):'.
-          format(len(completed), len(user_todos)))
-    [print('\t', todo.get('title')) for todo in completed]
-
-    with open(employee_id + '.csv', 'w') as csvfile:
-        writer = csv.writer(csvfile, lineterminator='\n',
-                            quoting=csv.QUOTE_ALL)
-        [writer.writerow(['{}'.format(field) for field in
-                          (todo.get('userId'), user.get('username'),
-                           todo.get('completed'), todo.get('title'))])
-         for todo in user_todos]
-
-if __name__ == '__main__':
-    api_request()
+if __name__ == "__main__":
+    employee_id = argv[1]
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                            format(employee_id))
+    json_user = json.loads(user.text)
+    username = json_user.get('username')
+    all_todo = 'https://jsonplaceholder.typicode.com/all_todo?userId={}'\
+        .format(employee_id)
+    todo_json = json.loads(user.text)
+    tasks = []
+    for task in todo_json:
+        tasks.append(task)
+    with open(sys.argv[1] + ".csv", "w") as f:
+        for task in tasks:
+            data = ['"' + sys.argv[1] + '"', '"' + username + '"', '"' +
+                    str(task.get('completed')) + '"', '"' + task.get('title') +
+                    '"']
+            f.write(",".join(data) + '\n')
